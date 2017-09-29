ad1cc2ba7c9549ccda976ac58e5c5d642366a1d6
Łukasz Gandecki <lgandecki@css.edu>
2017-09-29 12:19:46 +0400

    fixed wrong test case

--- tests/templateWithContentBlock.js
+++ tests/templateWithContentBlock.js
@@ -1,3 +1,12 @@
+Template.templateWithContentBlockOut.helpers({
+  list: () => [{something: 'args'}, {something: 'in'}, {something: 'list'}],
+  innerBlockData: () => [{arg: 'first'}, {arg: 'second'}]
+})
+
+Template.templateWithContentBlockInside.onCreated(function() {
+  console.log("WILK: this",this);
+})
+
 Template.templateWithContentBlockInside.helpers({
   myOwnHelper: function(arg1) {
     return `this is from my own helper ${arg1} ${Template.instance().data.someString}`
--- tests/templateWithContentBlockOut.js
+++
@@ -1,8 +0,0 @@
-Template.templateWithContentBlockOut.helpers({
-  list: () => [{something: 'args'}, {something: 'in'}, {something: 'list'}],
-  innerBlockData: () => [{arg: 'first'}, {arg: 'second'}]
-})
-
-Template.templateWithContentBlockInside.onCreated(function() {
-  console.log("WILK: this",this);
-})
